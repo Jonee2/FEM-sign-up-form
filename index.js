61bb2d9b70372c45bc564e34b04087fdf733fd11
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-02-22 11:13:32 -0300

    checkpoint

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-$(input)
+$('button').click(function () {
+    if ($('input').text('')) {
+    $('input')[2].attr('placeholder', 'email@example.com')
+    $('input').addClass('inputmissing')
+    $('.missing').css('display', 'initial')
+    }
+    else {
+        return;
+   }
+})
