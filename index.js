8db0dac0387c9be874718d55c9e370f352482983
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-02-24 22:23:03 -0300

    most validation done

--- index.js
+++ index.js
@@ -1,10 +1,85 @@
-$('button').click(function () {
-    if ($('input')) {
-    $('input')[2].attr('placeholder', 'email@example.com')
-    $('input').addClass('inputmissing')
-    $('.missing').css('display', 'initial')
+$(document).ready(function () {
+ 
+   
+    $(".firstmissing").hide();
+    let usernameError = true;
+    $("#firstname").keyup(function () {
+        validateFirstname();
+    });
+ 
+    function validateFirstname() {
+        let usernameValue = $("#firstname").val();
+        if (usernameValue.length == "") {
+            $(".firstmissing").show();
+            usernameError = false;
+            return false;
+        } else {
+            $(".firstmissing").hide();
+        }
     }
-    else {
-        return;
-   }
-})
+  $(".lastmissing").hide();
+    let lastnameError = true;
+    $("#lastname").keyup(function () {
+        validateLastname();
+    });
+ 
+    function validateLastname() {
+        let usernameValue = $("#lastname").val();
+        if (usernameValue.length == "") {
+            $(".lastmissing").show();
+            lastnameError = false;
+            return false;
+        } else {
+            $(".firstmissing").hide();
+        }
+    }
+     $(".emailmissing").hide();
+    let emailError = true;
+    $("#email").keyup(function () {
+        validateEmail();
+    });
+        const email = document.querySelector("#email");
+        email.addEventListener("blur", () => {
+            let regex =
+                /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
+            let s = email.value;
+            if (regex.test(s)) {
+                email.classList.remove(".emailmissing");
+                emailError = true;
+            } else {
+                email.classList.add(".emailmissing");
+                emailError = false;
+            }
+        });
+    $(".passwordmissing").hide();
+    let passwordError = true;
+    $("#password").keyup(function () {
+        validatePassword();
+    });
+    function validatePassword() {
+        let passwordValue = $("#password").val();
+        if (passwordValue.length == "") {
+            $(".passwordmissing").show();
+            passwordError = false;
+            return false;
+        } else {
+            $(".passwordmissing").hide();
+        }
+    }
+    $("button").click(function () {
+        validateLastname();
+        validateFirstname();
+        validatePassword();
+        validateEmail();
+        if (
+            usernameError == true &&
+            passwordError == true &&
+            confirmPasswordError == true &&
+            emailError == true
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    });
